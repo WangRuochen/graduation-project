e1d5b44badf912e5a1048f74bc8044c8ece2e450
wangruochen <771602374@qq.com>
2016-03-25 14:29:56 +0800

    修改com.aust.graproject文件名

--- src/main/java/com/aust/graproject/controller/MainController.java
+++ src/main/java/com/aust/graproject/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.aust.gradproject.controller;
+package com.aust.graproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
